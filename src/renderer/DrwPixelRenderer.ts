b305ecd08476578239c80e1bb7fd42fdb2a3c1b0
jaames <jamesdaniel2197@gmail.com>
2020-01-28 21:49:11 +0000

    implement layer pixel compositing, copying, etc

--- src/renderer/DrwPixelRenderer.ts
+++ src/renderer/DrwPixelRenderer.ts
@@ -22,6 +22,8 @@ export class DrwPixelRenderer extends DrwRendererBase<DrwPixelLayer> {
   private activeLayer: DrwPixelLayer;
   private pixels: Uint8ClampedArray;
 
+  private tmpPixelBuffer: Uint8ClampedArray;
+
   constructor(drw: DrwParser) {
     super(drw);
     this.brushEngine = new BrushEngine();
@@ -32,11 +34,17 @@ export class DrwPixelRenderer extends DrwRendererBase<DrwPixelLayer> {
     ctx.fillStyle = '#fff';
     ctx.fillRect(0, 0, this.width, this.height);
     const img = ctx.getImageData(0, 0, this.width, this.height);
-    const pixels = img.data;
-    const layerPixels = this.pixels;
-    console.log(layerPixels)
-    for (let o = 0; o < pixels.length; o++) {
-      pixels[o] = layerPixels[o];
+    const dst = img.data;
+    // const layerPixels = this.activeLayer.pixels;
+    for (let layerIndex = 4; layerIndex >= 0; layerIndex--) {
+      const layer = this.layers[layerIndex];const src = layer.pixels;
+      for (let o = 0; o < dst.length; o += 4) {
+        const a = src[o + 3] / 255;
+        dst[o + 0] = src[o + 0] + (1 - a) * dst[o + 0];
+        dst[o + 1] = src[o + 1] + (1 - a) * dst[o + 1];
+        dst[o + 2] = src[o + 2] + (1 - a) * dst[o + 2];
+        dst[o + 3] = src[o + 3] + (1 - a) * dst[o + 3];
+      }
     }
     ctx.putImageData(img, 0, 0);
   }
@@ -48,24 +56,58 @@ export class DrwPixelRenderer extends DrwRendererBase<DrwPixelLayer> {
   public setSize(width: number, height?: number) {
     super.setSize(width, height);
     this.pixels = new Uint8ClampedArray(this.width * this.height * DrwPixelLayer.numChannels);
+    this.tmpPixelBuffer = new Uint8ClampedArray(this.width * this.height * DrwPixelLayer.numChannels);
   }
 
   protected setLayer(layerIndex: number) {
     super.setLayer(layerIndex);
-    // this.activeLayer = this.layers[layerIndex];
-    // TODO implement layers
-    this.activeLayer = this.layers[0];
+    this.activeLayer = this.layers[layerIndex];
   }
 
-  protected copyLayer() {}
+  protected copyLayer(srcLayerIndex: number, dstLayerIndex: number) {
+    const isSrcLower = srcLayerIndex > dstLayerIndex; // (higher layer index value = lower layer)
+    const dstLayer = this.layers[dstLayerIndex];
+    const srcLayer = this.layers[srcLayerIndex];
+    const topLayer = isSrcLower ? dstLayer : srcLayer;
+    const lowLayer = isSrcLower ? srcLayer : dstLayer;
+    const dst = dstLayer.pixels;
+    const top = topLayer.pixels;
+    const low = lowLayer.pixels;
+    for (let o = 0; o < dst.length; o += 4) {
+      const a = top[o + 3] / 255;
+      dst[o + 0] = top[o + 0] + (1 - a) * low[o + 0];
+      dst[o + 1] = top[o + 1] + (1 - a) * low[o + 1];
+      dst[o + 2] = top[o + 2] + (1 - a) * low[o + 2];
+      dst[o + 3] = top[o + 3] + (1 - a) * low[o + 3];
+    }
+    // Mark layer as changed
+    this.layers[dstLayerIndex].hasChanged = true;
+  }
 
-  protected clearLayer() {}
+  protected clearLayer(layerIndex: number) {
+    const layer = this.layers[layerIndex]
+    layer.pixels.fill(0);
+    layer.hasChanged = true;
+  }
 
-  protected resetLayer() {}
+  protected resetLayer(layerIndex: number) {
+    this.clearLayer(layerIndex);
+  }
 
   protected updateBrush() {}
 
-  protected flip() {}
+  protected flip(flipX: boolean, flipY: boolean) {
+    const activeLayer = this.activeLayer;
+
+
+    TODO: this should flip all the pixels in every layer
+
+    // Copy layer pixels to temp buffer
+    this.tmpPixelBuffer.set(activeLayer.pixels);
+    for (let y = 0; y <= this.height; y++) {
+
+    }
+  }
 
   protected beginStroke(x: number, y: number, pressure: number) {
     this.brushEngine.drawBrush(this.userState, x, y, pressure);
@@ -78,9 +120,10 @@ export class DrwPixelRenderer extends DrwRendererBase<DrwPixelLayer> {
 
   protected finalizeStroke() {
     const dirtyRegion = this.userState.dirtyRegion;
+    const activeLayer = this.activeLayer;
     if (dirtyRegion.hasChanged) {
-      this.brushEngine.compositeIntoPixelBuffer(this.userState, this.pixels);
-      this.activeLayer.hasChanged = true;
+      this.brushEngine.compositeIntoPixelBuffer(this.userState, activeLayer.pixels);
+      activeLayer.hasChanged = true;
       // clear alpha buffer
       this.userState.alphaBuffer.fill(0);
       dirtyRegion.reset();
